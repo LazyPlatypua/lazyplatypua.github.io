21df28effd71fdbbfb20072fe575bc3b61f3d4bb
LazyPlatypua <mish.nazarov2013@yandex.ru>
2025-01-08 20:42:52 +0300

    Пересборка

--- miniature-painting/bases/toc.js
+++ miniature-painting/bases/toc.js
@@ -1,1 +1,1 @@
-window.__DATA__.data.toc = {"title":"Оформление баз","items":[{"name":"Обзор","href":"bases\\index.html","id":"Обзор-0-0.383241248133392"}]};
+window.__DATA__.data.toc = {"title":"Оформление баз","items":[{"name":"Обзор","href":"bases\\index.html","id":"Обзор-0-0.13228358310293942"}]};
