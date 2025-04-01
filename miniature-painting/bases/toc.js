0f3ddbbf08e00421e77df30d11960f244e0656d6
LazyPlatypua <mish.nazarov2013@yandex.ru>
2025-04-01 19:30:45 +0300

    Добавлена инструкция для сестры битвы

--- miniature-painting/bases/toc.js
+++ miniature-painting/bases/toc.js
@@ -1,1 +1,1 @@
-window.__DATA__.data.toc = {"title":"Оформление баз","items":[{"name":"Обзор","href":"bases\\index.html","id":"Обзор-0-0.31418616292012413"},{"name":"Джунгли","href":"bases\\jungle.html","id":"Джунгли-1-0.47100979289425005"}]};
+window.__DATA__.data.toc = {"title":"Оформление баз","items":[{"name":"Обзор","href":"bases\\index.html","id":"Обзор-0-0.23411403630499317"},{"name":"Джунгли","href":"bases\\jungle.html","id":"Джунгли-1-0.9168422207516167"}]};
