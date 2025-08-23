037f9e36df10dd2761aee3f741d335f8b5e760b7
LazyPlatypua <mish.nazarov2013@yandex.ru>
2025-08-23 09:24:33 +0300

    удаление папки m-p

--- miniature-painting/leather/toc.js
+++
@@ -1,1 +0,0 @@
-window.__DATA__.data.toc = {"name":"Кожа (материал)","href":"leather\\index.html","items":[{"name":"Коричневая","href":"leather\\brown.html","id":"Коричневая-0-0.3211620451181152"},{"name":"Красная","href":"leather\\red.html","id":"Красная-1-0.8381231608582587"}]};
